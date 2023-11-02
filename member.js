e5d7dbd9f86827a290ab0c6ba21a61d72497672e
jissart1 <jissart1@users.noreply.github.com>
2023-11-02 15:03:18 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,17 @@
+function skillMember() {
+  return {
+    name: "Member",
+    icon: "icons/svg/lorc/king.svg",
+    subtitle: "You are a member of a group or organization",
+    description: "You are a member of a group or organization",
+    reference: "PHB, pg. 185.",
+    bullets: [
+      "You have a position of authority and responsibility in that group",
+      "Your rank can grant you access to places where commoners and low-ranking soldiers aren't allowed",
+      "You can invoke your rank to influence other members of your organization, including people of higher rank",
+      "Your fellow members expect you to obey your leaders, and you are expected to obey your leaders' orders",
+      "You can expect to be targeted by enemies of your organization eventually",
+      "If you have a good relationship with your organization, your allies within it might help you survive in the wilderness or to find information you need"
+    ]
+  };
+}
